e2ffa0a30ba4ae6c61b84abff9cfa73af2697c70
Sam Coy <samcoy3@gmail.com>
2021-12-05 12:17:28 +0000

    Day 4 solutions

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,19 +1,20 @@
 module Days.Day04 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Control.Applicative ((<|>), many)
+import Data.List ( find, scanl', transpose )
+import Data.Maybe ( fromJust )
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+    ( Parser,
+      count,
+      sepBy,
+      sepBy1,
+      decimal,
+      skipSpace,
+      char,
+      endOfLine )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,62 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (,) <$> (calledNumbers <* count 2 endOfLine) <*> bingoCards
+  where
+    calledNumbers = decimal `sepBy` char ','
+    bingoCards =
+      count 5 (count 5 $ skipSpace *> ((,False) <$> decimal))
+        `sepBy1` count 2 endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+-- Representing a bingo card as a 2D list of pairs of ints and bools.
+-- The int represents the number in the square; the bool indicates if it's been called.
+type BingoCard = [[(Int, Bool)]]
 
-type OutputA = Void
+type Input = ([Int], [BingoCard])
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+-- Given a bingo card, checks whether it is complete.
+bingoCardComplete :: BingoCard -> Bool
+bingoCardComplete card =
+  any (and . fmap snd) card
+    || any (and . fmap snd) (transpose card)
+
+-- Given a number called out and a bingo card, fills in that number on the card.
+callNumberOnCard :: Int -> BingoCard -> BingoCard
+callNumberOnCard number =
+  fmap
+    ( fmap
+        (\square -> if square == (number, False) then (number, True) else square)
+    )
+
+-- Plays a bingo game.
+-- Input is the sequence of numbers to be called and a list of bingo cards.
+-- Returns a list of turns of bingo. Each card is removed the turn after it wins.
+bingoGame :: Input -> [(Int, [BingoCard])]
+bingoGame (calledNumbers, cards) =
+  zip calledNumbers . tail $
+    scanl' (\cards number -> callNumberOnCard number <$> filter (not . bingoCardComplete) cards) cards calledNumbers
+
+-- Scores a bingo card according to the definition.
+cardScore :: Int -> BingoCard -> Int
+cardScore numberCalled card =
+  (*) numberCalled $
+    sum . fmap fst . filter (not . snd) . concat $ card
+
+-- Plays a game of bingo, stopping the game and scoring any winning cards once a condition is met.
+playGameUntilCardsComplete :: ((BingoCard -> Bool) -> [BingoCard] -> Bool) -> Input -> Int
+playGameUntilCardsComplete condition input = sum . fmap (cardScore lastNumber) . filter bingoCardComplete $ cards
+  where
+    (lastNumber, cards) = fromJust . find (condition bingoCardComplete . snd) . bingoGame $ input
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = playGameUntilCardsComplete any
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = playGameUntilCardsComplete all
