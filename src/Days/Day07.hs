eedd3dbfc964cf0a9e534c43574544141955331e
Sam Coy <samcoy3@gmail.com>
2021-12-07 09:08:37 +0000

    Day 7 solutions

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,19 +1,10 @@
 module Days.Day07 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Data.List ( sort )
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text ( decimal, sepBy, char, Parser )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +12,26 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` char ','
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA crabs =
+  let median = sort crabs !! (length crabs `div` 2)
+   in sum $ fmap (abs . subtract median) crabs
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB crabs =
+  let mean = sum crabs `div` length crabs
+      fuelUsage distance = (distance * (distance + 1)) `div` 2
+   in min
+        (sum $ fmap (fuelUsage . abs . subtract mean) crabs)
+        (sum $ fmap (fuelUsage . abs . subtract (mean + 1)) crabs)
