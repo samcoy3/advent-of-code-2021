ff63fc9d86fcdcfe8bf0b8a566621fe80e925ae3
Sam Coy <samcoy3@gmail.com>
2021-12-08 10:42:28 +0000

    Day 8 solutions

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -10,6 +10,7 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Util.Parsers (around)
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
@@ -21,19 +22,68 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (word `sepBy1` char ' ' `around` string " | ") `sepBy1` endOfLine
+  where
+    word = Set.fromList <$> many1 letter
 
 ------------ TYPES ------------
-type Input = Void
+type Display = ([Set Char], [Set Char])
 
-type OutputA = Void
+type Input = [Display]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . filter (`elem` [2, 3, 4, 7]) . fmap Set.size . concatMap snd
 
 ------------ PART B ------------
+deduce :: Display -> Int
+deduce (signals, output) =
+  let -- First we identify the digits with a unique number of segments.
+      one = head . filter ((== 2) . length) $ signals
+      seven = head . filter ((== 3) . length) $ signals
+      four = head . filter ((== 4) . length) $ signals
+      eight = head . filter ((== 7) . length) $ signals
+
+      -- Next, we notice that of the digits with five segments, three is the only one which fully contains the digit 1.
+      three =
+        head . filter ((== 2) . length . (`Set.intersection` one))
+          . filter ((== 5) . length)
+          $ signals
+
+      -- Next we handle the digits with six segments.
+      -- 6 is the only digit missing a segment which is present in 1.
+      -- 0 only contains three of the segments in 4. 6 does too, but we've found it already, so we can rule it out.
+      -- 9 is the only remaining digit with six segments.
+      six =
+        head . filter ((== 1) . length . (`Set.intersection` one))
+          . filter ((== 6) . length)
+          $ signals
+      zero =
+        head . filter (/= six) . filter ((== 3) . length . (`Set.intersection` four))
+          . filter ((== 6) . length)
+          $ signals
+      nine =
+        head . filter (`notElem` [six, zero])
+          . filter ((== 6) . length)
+          $ signals
+
+      -- Finally, 2 and 5 are differentiable by their overlap with the digit 4.
+      -- 2 overlaps with 4 in only two places (5 and 3 overlap with 4 in three places).
+      -- Finally, 5 is the remaining digit with five segments.
+      two =
+        head . filter ((== 2) . length . (`Set.intersection` four))
+          . filter ((== 5) . length)
+          $ signals
+      five = head . filter (`notElem` [two, three]) . filter ((== 5) . length) $ signals
+
+      segmentMap =
+        Map.fromList . flip zip [0 .. 9] $
+          [zero, one, two, three, four, five, six, seven, eight, nine]
+   in foldl1' (\a x -> a * 10 + x) $ (segmentMap Map.!) <$> output
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . fmap deduce
