900a819dd9eb29be6cb9b0df1947ce323fdeacb4
Sam Coy <samcoy3@gmail.com>
2021-12-15 19:21:18 +0000

    Day 11 solutions

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,19 +1,18 @@
 module Days.Day11 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+{- ORMOLU_DISABLE -}
+import Data.List ( (\\), elemIndex)
+import Data.List.NonEmpty ( toList, unfoldr )
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
+import Data.Maybe (fromJust)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Parsers as P ( coordinateParser )
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text ( Parser )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +20,53 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = P.coordinateParser (pure . Energy . read . pure) 0
 
 ------------ TYPES ------------
-type Input = Void
+data EnergyLevel = Energy Int | Flashed deriving (Show, Eq)
 
-type OutputA = Void
+type Input = Map (Int, Int) EnergyLevel
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+increaseEnergy :: EnergyLevel -> EnergyLevel
+increaseEnergy (Energy x) = if x < 9 then Energy (x + 1) else Flashed
+increaseEnergy Flashed = Flashed
+
+neighbours :: (Int, Int) -> [(Int, Int)]
+neighbours (x, y) = [(x', y')
+                    | x' <- [x-1..x+1], y' <- [y-1..y+1]
+                    , (x, y) /= (x', y')]
+
+step :: Input -> (Int, Input)
+step cavern =
+  let
+      -- Reset flashes at the end of a step
+      resetFlashes o = if o == Flashed then Energy 0 else o
+      -- At the start of the step, increases the energy of all octopodes
+      increasedAllEnergy = increaseEnergy <$> cavern
+      -- The flashes which are present after increasing the energy levels
+      startingFlashes =
+        Set.fromList . Map.keys $
+          Map.filter (== Flashed) increasedAllEnergy
+      -- A flooding algorithm to propagate the flashing.
+      -- seenFlashes is a set containing all the nodes that have flashed this step.
+      -- recentFlashes only contains nodes which flashed in the most recent epoch of the flooding algorithm.
+      step' seenFlashes recentFlashes cavern =
+        if Set.null recentFlashes
+          then (length seenFlashes, resetFlashes <$> cavern)
+          else
+            let triggerFlashes = foldr (Map.adjust increaseEnergy) cavern (concat $ Set.map neighbours recentFlashes)
+                flashes = Set.fromList . Map.keys $ Map.filter (== Flashed) triggerFlashes
+             in step' flashes (flashes Set.\\ seenFlashes) triggerFlashes
+   in step' startingFlashes startingFlashes increasedAllEnergy
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . take 100 . toList . unfoldr (fmap pure . step)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = (+1) . fromJust . elemIndex 100 . toList . unfoldr (fmap pure . step)
