1b91a11dc2d96e68e4e851ad812247ddecee530b
Sam Coy <samcoy3@gmail.com>
2021-12-15 20:07:22 +0000

    Day 12 solutions

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -1,39 +1,69 @@
 module Days.Day12 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
+import Data.Char (isLower)
+import Data.Functor (($>))
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Util.Parsers (around)
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+    ( Parser, choice, many1, endOfInput, letter, char, endOfLine )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+-- Construct an adjacency list from our input
+-- Note: it's important to delete incoming edges to "start":
+--   we never want to take them (including in Part B)
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = fmap (Set.delete "start") <$>
+  choice
+    [ endOfInput $> Map.empty,
+      edge <* endOfLine >>= \(u, v) ->
+        Map.insertWith Set.union u (Set.singleton v)
+          . Map.insertWith Set.union v (Set.singleton u)
+          <$> inputParser
+    ]
+  where
+    edge = many1 letter `around` char '-'
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Graph
+type Graph = Map Node (Set Node)
+type Node = String
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+-- A parallel "bfs"-like algorithm for path enumeration.
+bfs :: Bool -> Graph -> [[Node]]
+bfs canVisitSmallCave graph = bfs' canVisitSmallCave Set.empty "start"
+  where
+    bfs' canVisitSmallCave excluded current =
+          let newExcluded = if isLower (head current) then Set.insert current excluded else excluded
+              validNeighbours = (graph Map.! current) Set.\\ excluded
+              secondSmallCaves = if canVisitSmallCave
+                then (graph Map.! current) `Set.intersection` excluded
+                else Set.empty
+           in if current == "end"
+                 then [["end"]]
+                 else fmap (current :) . concat
+                      $ (bfs' canVisitSmallCave newExcluded
+                      <$> Set.toList validNeighbours)
+                      ++ (bfs' False newExcluded
+                      <$> Set.toList secondSmallCaves)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . bfs False
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = length . bfs True
